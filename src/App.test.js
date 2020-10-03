a5cfe13252b7abf4093c03eb237ecbce39242dbd
PiotrAleksander <piotrhugonow@gmail.com>
2020-10-03 17:50:42 +0200

    cleanup and libraries

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders GH Users Explorer paragraph', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const paragraphElement = getByText(/GH Users Explorer/i);
+  expect(paragraphElement).toBeInTheDocument();
 });
